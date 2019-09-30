c7623aa7f7882bf01970b0142438e1f7c97bbdbb
Ike <ikeholzmann@gmail.com>
2019-09-30 14:28:46 -0700

    Playlist data to client & stretch challenge

--- app.py
+++ app.py
@@ -3,8 +3,33 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+    {
+    'title': 'Cat Videos',
+    'description': 'Cats acting weird',
+    'videos': [
+        {
+        'name': 'Cat Holds a Hotdog',
+        'url': 'https://www.youtube.com/watch?v=j8rrCHWM-W8'
+        },
+        {
+        'name': 'Cats React to Fake Cats',
+        'url': 'https://www.youtube.com/watch?v=SAjs3SdwTAk'
+        }
+    ]
+    },
+    { 'title': '80\'s Music',
+    'description': 'Don\'t stop believing!',
+    'videos': [
+        {
+        'name': 'a-Ha - Take on Me',
+        'url': 'https://www.youtube.com/watch?v=djV11Xbc914'
+        },
+        {
+        'name': "Bon Jovi - Livin' on a Prayer",
+        'url': 'https://www.youtube.com/watch?v=lDK9QqIzhwk'
+        }
+    ]
+    }
 ]
 
 @app.route('/')
